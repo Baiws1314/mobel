9ee5aef859d85d96433e3481b82aea4276244537
Baiws1314 <1756805854@qq.com>
2019-01-07 09:37:43 +0800

    add all all

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="hello">
-    <mt-button size="normal" type='danger'>normal</mt-button>
+    <mt-header fixed title="首页" style="height:50px;background:#ccc;color:#000;font-size:16px"></mt-header>
+    <section>
+      <router-view></router-view>
+    </section>
+    <footer>
+      <router-link to="/home" tag="span" class="">首页</router-link>
+      <router-link to="/strategy" tag="span">攻略</router-link>
+      <router-link to="/cart" tag="span">购物车</router-link>
+      <router-link to="/my" tag="span">我的</router-link>
+    </footer>
   </div>
 </template>
 
@@ -12,5 +21,27 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.router-link-active{
+  color: #000;
+}
+.hello{
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+section{
+  flex: 1;
+  margin-top:50px;
+  overflow: auto;
+}
+footer{
+  height: 50px;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #686868;
+  background: #ccc;
+}
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Home from '@/components/Home'
+// import Strategy from '@/components/Strategy'
+// import Cart from '@/components/Cart'
+// import My from '@/components/My'
 Vue.use(Router)
 
 export default new Router({
@@ -9,7 +12,26 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      redirect:'/home',
+      children:[
+        {
+          path:'/home',
+          component:Home,
+        },
+        // {
+        //   path:'/strategy',
+        //   component:Strategy
+        // },
+        // {
+        //   path:'/cart',
+        //   component:Cart
+        // },
+        // {
+        //   path:'/my',
+        //   component:My
+        // }
+      ]
     }
   ]
 })
